4a879f469ae4fd7be31d3787a4e578a8f9b61102
danielNewaz <danielnwz420@gmail.com>
2022-04-17 13:20:07 -0700

    reset-pass

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,10 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from './../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { async } from '@firebase/util';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -19,6 +20,8 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
     if(user){
         navigate(from, {replace:true});
@@ -39,6 +42,12 @@ const Login = () => {
         navigate('/register')
     }
 
+    const resetPassword = async()=>{
+        const email = emailRef.current.value;
+        await sendPasswordResetEmail(email);
+        alert('Sent email');
+    }
+
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-danger text-center mt-2'>Please Login</h2>
@@ -53,15 +62,14 @@ const Login = () => {
                     
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+                
+                <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+                    Login
                 </Button>
             </Form>
             {errorElement}
-            <p>New to My site ? <Link to="/register" className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}> Please register</Link></p>
+            <p>New to My site ? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}> Please register</Link></p>
+            <p>Forget Password ? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={resetPassword}> Forget Password</Link></p>
             <SocialLogin></SocialLogin>
         </div>
     );
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
@@ -6,6 +6,7 @@ import auth from './../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
@@ -28,8 +29,12 @@ const Register = () => {
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+        // const agree = event.target.terms.checked;
+        if(agree){
+            createUserWithEmailAndPassword(email, password)
+        }
 
-        createUserWithEmailAndPassword(email, password)
+       
     }
 
     return (
@@ -41,10 +46,13 @@ const Register = () => {
                 <input type="email" name="email" id="" placeholder="Email Address" required />
             
                 <input type="password" name="password" id="" placeholder='Password' required />
-
-                <input type="submit" value="Register" />
+                <input onClick={() =>setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={agree ? 'ps-2 text-primary' : 'ps-2 text-danger'} htmlFor="terms">Accept Terms and Conditions</label>
+                <input
+                disabled={!agree}
+                 className='w-50 mx-auto btn btn-primary mt-2' type="submit" value="Register" />
             </form>
-            <p>Already have an account ? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}> Please Login</Link></p>
+            <p>Already have an account ? <Link to="/login" className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}> Please Login</Link></p>
             <SocialLogin></SocialLogin>
         </div>
     );
